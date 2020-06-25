68e3b44f7eff60e23aeccf037e761fb4b80b0330
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-06-25 12:48:21 +0200

    Strucural directive if and else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public title: string = 'Event Manager';
+  private _title: string = 'Event Manager';
+
+  private loggedIn: boolean = true;
+
+  /**
+   *
+   */
+  public get isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+  public set isLoggedIn(isLoggedIn: boolean ) {
+    this.loggedIn = isLoggedIn;
+  }
+
+  public get title(): string {
+    return this._title;
+  }
+
+  public set title(title: string) {
+    this._title = title;
+  }
+
+  public getLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+  public toggleLogStatus(): void {
+    this.loggedIn = !this.loggedIn;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitlePipe } from './pipes/title.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlePipe
   ],
   imports: [
     BrowserModule,
